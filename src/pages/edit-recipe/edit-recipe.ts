539f52e983eb30719c08b7c3cc7a9249f5bb07dc
fengjie <1369431567@qq.com>
2018-05-20 11:44:01 +0800

    first commit

---
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {  NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-edit-recipe',
+  templateUrl: 'edit-recipe.html',
+})
+export class EditRecipePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditRecipePage');
+  }
+
+}
---
+++ src/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {  NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-shopping-list',
+  templateUrl: 'shopping-list.html',
+})
+export class ShoppingListPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ShoppingListPage');
+  }
+
+}
